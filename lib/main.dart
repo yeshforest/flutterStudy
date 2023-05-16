c2ac3f7d644f4872b6e100eead87bce2e1d2fcd4
haerimRoh <mtxpvksh@gmail.com>
2023-05-16 15:10:06 +0900

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/screens/home_screen.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -9,17 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Subpage Example',
-        theme: ThemeData(
-          backgroundColor: const Color(0xFFE7626C),
-          textTheme: const TextTheme(
-            headline1: TextStyle(
-              color: Color(0xFF232B55),
-            ),
-          ),
-          cardColor: const Color(0xFFF4EDDB),
-        ),
-      home:const HomeScreen(),
+       home:Container(),
     );
   }
 }
