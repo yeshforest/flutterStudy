71f8f6d660c20f65101a8a982800e4ff58a8a5d2
haerimRoh <mtxpvksh@gmail.com>
2023-05-17 23:50:56 +0900

    [feat] add getTodayToons api function

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
+import 'package:toonflix/services/api_service.dart';
 
 
 void main() {
+  ApiService().getTodayToons();
   runApp(const MyApp());
 }
 
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,17 @@
-class ApiService{
-  final String baseUrl="https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today="today";
+import 'package:http/http.dart' as http;
 
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+
+  void getTodayToons() async{
+    final url = Uri.parse('$baseUrl/$today');
+    final response =await http.get(url);
+    if(response.statusCode ==200){
+      print(response.body);
+      return;
+    }
+    throw Error();
+  }
 }
